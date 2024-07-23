5c4850a65bae38158465b6968aadaf30ad8ba51c
Wojciech Mazur <odinkodilla@int.pl>
2024-07-23 09:59:25 +0200

    Create Class Card

--- Exercise_7_3.py
+++ Exercise_7_3.py
@@ -1,1 +1,17 @@
+from faker import Faker
+fake = Faker('pl_PL')
+
+class Card:
+    def __init__(self, first_name, last_name, company, job, address, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.company = company
+        self.job = job
+        self.address = address
+        self.email = email
+
+    @property
+    def label_length(self):
+    return len(self.first_name + ' ' + self.last_name)
+
 
